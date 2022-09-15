c21f02e337d8ec2f9f985978c6e804e8b28c4576
Rômulo Menezes de Santana <romulomsantana@hotmail.com>
2022-09-15 11:48:31 -0300

    first commit

---
+++ CalculadoraIMC/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  CalculadoraIMC
+//
+//  Created by Student on 13/09/22.
+//  Copyright © 2022 Student. All rights reserved.
+//
+
+import UIKit
+
+//let numberFormatter = NumberFormatter()
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nomePesoText: UITextField!
+    
+    @IBOutlet weak var nomeAlturaText: UITextField!
+    
+    @IBOutlet weak var resultadoLabel: UILabel!
+    
+    @IBAction func nomeCalculoButton(_ sender: Any) {
+        
+        
+        let peso = Double(nomePesoText.text!)!
+        let altura = Double(nomeAlturaText.text!)!
+        let calculo = peso/(altura*altura)
+        let calculoFormatado = String(format: "Resultado: %.2f", calculo)
+        
+        resultadoLabel.text = String(calculoFormatado)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
